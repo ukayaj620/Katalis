ec0cf962d1a8ff149312b7559be9904a7f410ba9
ukayaj620 <jayakubriliantio@gmail.com>
2019-10-14 12:48:51 +0700

    procedure for words stemmer and stopwords removal

---
+++ preprocessing.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+
+factoryStem = StemmerFactory()
+stemmer = factoryStem.create_stemmer()
+
+factoryStop = StopWordRemoverFactory()
+stopper = factoryStop.create_stop_word_remover()
+
+xData = []
+yData = []
+
+rawDatasets = pd.read_csv('dataset\RawDatasets.csv',delimiter=',')
+
+count = 0
+
+for k in rawDatasets['Kalimat']:
+    if count % 100 == 0:
+        print(count)
+    sentStemmed = stemmer.stem(k)
+
+    sentStopped = sentStemmed
+    temp = stopper.remove(k)
+
+    while temp != sentStopped:
+        sentStopped = temp
+        temp = stopper.remove(sentStopped)
+
+    xData.append(sentStopped)
+    count += 1
+
+for k in rawDatasets['Formalitas']:
+    yData.append(k)
+
+processDatasets = { 'Kalimat' : xData,
+                    'Formalitas' : yData
+                    }
+
+dataFrameCSV = pd.DataFrame(processDatasets, columns=['Kalimat', 'Formalitas'])
+
+dataFrameCSV.to_csv('D:\BIOS-Hackaton\Katalis\dataset\processDatasets.csv', index=None, header=True)
+
+print(dataFrameCSV)
