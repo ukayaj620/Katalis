46ddda66cf4eb75e4ebcbb40525910e3b323c2d7
ukayaj620 <jayakubriliantio@gmail.com>
2019-10-14 21:43:01 +0700

    MainTraining

--- main_traning.py
+++ main_traning.py
@@ -2,7 +2,9 @@ from analiser import Analiser
 
 an = Analiser()
 
-an.train(output_file='model_1')
+an.train(output_file='model')
 
-test_1 = "gw sk mkn"
-print(an.testFromTrained([an.tfidf_data.transform([test_1])]))
+while True:
+    sentence = input()
+    print(an.testFromTrained([an.tfidf_data.transform(sentence)]))
+
