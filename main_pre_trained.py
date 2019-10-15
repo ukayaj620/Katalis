07062a7595e593a233bb8066a710ef220ad81994
ukayaj620 <jayakubriliantio@gmail.com>
2019-10-15 09:27:24 +0700

    minor change for testing the model

--- main_pre_trained.py
+++ main_pre_trained.py
@@ -1,8 +1,18 @@
 from analiser import Analiser
 
+# FIRST_MODEL pool1 0.005 2 20
+# SECOND_MODEL pool2 0.005 2 20
+# THIRD_MODEL pool2 0.01 16 25
+# FOURTH_MODEL pool2 0.01 16 40
+# FIFTH_MODEL pool2 0.01 32 45
+# SIXTH_MODEL pool2 0.02 16 25
+# SEVENTH_MODEL pool2 0.025 16 20
+# EIGHTH_MODEL pool2 0.025 16 20 softmax
+
+
 an = Analiser()
 
-an.load_model(file_name='model_5')
+an.load_model(file_name='EIGHT_MODEL')
 
 while True:
     sentence = input()
