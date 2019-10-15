4810f9b0ca74ae33e2249f46218c11be452924ea
ukayaj620 <jayakubriliantio@gmail.com>
2019-10-15 09:25:13 +0700

    adding feature in the interface

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ client = discord.Client()
 
 print("Loading Model...")
 an = Analiser()
-model = an.load_model('model_3')
+model = an.load_model('EIGHT_MODEL')
 
 
 @client.event
@@ -27,11 +27,19 @@ async def on_message(message):
 
     y1 = y[0][0]
     y2 = y[0][1]
+
+    if y1 > 0.99999:
+        y1 = 11
+        y2 = 0
+    elif y1 < 0.00001:
+        y1 = 0
+        y2 = 11
+
     while math.floor(y1) == math.floor(y2):
         y1 = y1*10 - math.floor(y1)*10
         y2 = y2*10 - math.floor(y2)*10
 
-    response = "|"
+    response = "Jayaku's Bot\n|"
     for i in range(11):
         if(i < y1):
             response += ":white_large_square:"
